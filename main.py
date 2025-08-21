6fbafec2bcf08d5b86aef01ebdd0ce054fe7fe99
gelik <136448734+mysitessa@users.noreply.github.com>
2025-08-21 15:24:51 +0300

    add top maps

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from data.db_session import create_session, global_init
 from data.models import InitGames, Items, Weapons, Matches, Clients, Events, ItemPickups, Kills
 from flask import Flask, jsonify
 from flask_cors import CORS
+from collections import Counter
 
 
 app = Flask(__name__)
@@ -28,7 +29,24 @@ def get_matches():
     return jsonify({"matches":data})
 
 
+@app.route('/top_maps')
+def get_top_maps():
+    map_names = [i[0] for i in session.query(InitGames.map_name).all()]
+    maps_count = Counter(map_names)
+    maps_count = maps_count.most_common()
+    #print("maps_count:", maps_count)
     
+    while len(maps_count) < 3:
+        maps_count.append(("не занято", 0))
+    
+    sp_most_popular_maps = [f"{i[0]} - {i[1]}" for i in maps_count[:3]]
+    #print("sp_most_popular_maps", sp_most_popular_maps)
+    
+    return jsonify({
+        "1.": sp_most_popular_maps[0],
+        "2.": sp_most_popular_maps[1],
+        "3.": sp_most_popular_maps[2]
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
