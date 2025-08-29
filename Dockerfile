cf9daf7eb9fd65f120b741ddecf5ed4621101e89
gelik <136448734+mysitessa@users.noreply.github.com>
2025-08-29 12:19:27 +0300

    work docker

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,20 @@
-FROM python:3.11-slim
+FROM python:3.10-slim
 
-ENV PYTHONDONTWRITEBYTECODE=1
-ENV PYTHONUNBUFFERED=1
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    libpq-dev \
+    && rm -rf /var/lib/apt/lists/*
+
 
 WORKDIR /app
+
 COPY requirements.txt .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . .
 
-EXPOSE 5000
+ENV FLASK_APP=main.py
+ENV FLASK_RUN_HOST=0.0.0.0
 
-CMD ["python", "main.py"]
+CMD ["flask", "run", "--host=0.0.0.0", "--port=5000"]
--- data/db_session.py
+++ data/db_session.py
@@ -1,3 +1,4 @@
+import os
 import sqlalchemy as sa
 import sqlalchemy.orm as orm
 from sqlalchemy.orm import Session
@@ -9,18 +10,19 @@ __factory = None
 
 def global_init():
     global __factory
-
     if __factory:
         return
 
-
-    conn_str = 'postgresql+psycopg2://postgres:postgres@localhost:5432/postgres'
+    # Берём строку подключения из переменной окружения
+    conn_str = os.getenv(
+        "DATABASE_URL",
+        "postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
+    )
 
     print(f"Подключение к базе данных по адресу {conn_str}")
 
     engine = sa.create_engine(conn_str, echo=False)
     __factory = orm.sessionmaker(bind=engine)
-
 
     SqlAlchemyBase.metadata.create_all(engine)
 
